a925c580f7029b39592394b61d20efbd1790d80b
Aurore <aurore.f.comte@gmail.com>
2017-01-27 09:57:36 +0100

    Ajout de commentaires

--- Fonctions1.R
+++ Fonctions1.R
@@ -1,5 +1,6 @@
-##Change un arbre en liste de matrices
 
+##----trees2matrices.Distatis change un arbre en liste de matrices----
+#Fonction modifiée à partir de celle de pMCOA
 trees2matrices.Distatis<-function(trees, distance="nodal",bvalue=0) {
   ##progress bar
   print("Conversion from trees to matrices:")
@@ -44,11 +45,13 @@ trees2matrices.Distatis<-function(trees, distance="nodal",bvalue=0) {
   }
   close(pb)
   TRS<-lapply(list.trees, cophenetic)
+###On attribut ici de numéro d'un arbre à chaque élément de la liste. (Utile pour la fonction mat2Dist)
   names(TRS)<-as.list(labels(trees))
   return(TRS)
 }
 
-#Applique distatis sur une liste de matrice de distance
+##----mat2Dist applique distatis sur une liste de matrice de distance----
+#En premier lieu, la liste de matrice est changée en cube
 mat2Dist <-function(matrices){
   genesNumber=length(matrices)
   speciesNumber=nrow(matrices[[genesNumber]])
@@ -56,23 +59,26 @@ mat2Dist <-function(matrices){
   for (i in 1:genesNumber){
 	  TheVeryBigCube[,,i]<-matrices[[i]]
   }
+  #On garde les identifiants de chaque éléments de la matrice dans le cube
 	rownames(TheVeryBigCube) <- rownames(matrices[[i]])
 	colnames(TheVeryBigCube) <- colnames(matrices[[i]])
+	#On applique distatis sur le cube obtenu
   testDistatis1 <- distatis(TheVeryBigCube)
+  #On fait suivre le numéro de chaque élement de la matrice de départ aux résultats qui nous interressent
   dimnames(testDistatis1$res4Splus$PartialF)[[3]] = names(matrices)
   return(testDistatis1)
 }
 
-#créé la matrice 2WR a partir des résultats de distatis
+###----créé la matrice 2WR à partir des résultats de distatis----
 Dist2WR <-function(testDistatis2){
-  
+  #Dist.TL = Matrice gène/espèce
   Dist.TL = matrix(,dim(testDistatis2$res4Splus$PartialF)[3]*nrow(testDistatis2$res4Splus$PartialF),2)
   colnames(Dist.TL)=c("T","L")
-  
+  #Dist.TLi = Matrice des coordonnées dans les différents axes de tous les couples gène/espèce
   Dist.TLi = matrix(,dim(testDistatis2$res4Splus$PartialF)[3]*nrow(testDistatis2$res4Splus$PartialF),ncol(testDistatis2$res4Splus$PartialF))
-  
   colnames(Dist.TLi)=colnames(testDistatis2$res4Splus$PartialF)
   ListRowNames = list()
+  #Remplissage des 2 matrices Dist.TL et Dist.TLi
   j=1
   for (i in 1:dim(testDistatis2$res4Splus$PartialF)[3]) {
     if (j < nrow(Dist.TLi)+1) {
@@ -96,37 +102,40 @@ Dist2WR <-function(testDistatis2){
   matrixWR<-matrix(ncol=dim(testDistatis2$res4Splus$PartialF)[3], nrow=nrow(testDistatis2$res4Splus$PartialF))
   rownames(matrixWR)=rownames(testDistatis2$res4Splus$PartialF)
   array.vector<-array()
+  ##MatrixDif est la matrice des difference au carré entre les coordonnées de chaque couple gène/espèce et les coordonnées moyenne du gène
   matrixDif<-matrix(ncol=ncol(Dist.TLi), nrow=nrow(Dist.TLi))
   for (i in 1:nrow(matrixDif)){
     matrixDif[i,]<-(Dist.TLi[i,]-testDistatis2$res4Splus$F[as.character(Dist.TL[i,2]),])^2
+    ##On fait la racine carré de la somme de toutes les différences au carré pour chaque couple gène/espèce et ont les met toutes dans le vecteur suivant
     array.vector[i]<-sqrt(sum(matrixDif[i,]))
   }
+  #Remplissage de la matrice 2WR avec les données du vecteurs
   colnamesMatrixWR=list()
   for (i in 1:ncol(matrixWR)) {
     matrixWR[,i]<-array.vector[as.character(Dist.TL[,1])==as.character(dimnames(testDistatis2$res4Splus$PartialF)[[3]][i])]
+    #On retrouve le numéro de chaque gène correspondant à chaque distance
     colnamesMatrixWR[[i]]=dimnames(testDistatis2$res4Splus$PartialF)[[3]][i]
   }
   colnames(matrixWR)=colnamesMatrixWR
-  
   return(matrixWR)
 }
 
-#Suppression des cell outiers dans les arbres du départ pour ensuite faire la détection des cell outliers
-
+###-----Suppression des complete outiers dans les arbres du départ pour ensuite faire la détection des cell outliers-----
+#Fonction modifiée à partir de celle de pMCOA
 rm.gene.and.species.Distatis<-function(trees, sp2rm, gn2rm) {
   gene.names = list()
   for (i in 1: length(labels(trees))){
     gene.names[i]=labels(trees)[i]
   }
-  if (length(CompleteOutlierDist$outsp)>0) {
+  if (length(sp2rm)>0) {
     for (i in 1:length(trees)) {
       sp<-trees[[i]]$tip.label
-      toremove<-intersect(sp, CompleteOutlierDist$outsp)
+      toremove<-intersect(sp, sp2rm)
       trees[[i]]<-drop.tip(trees[[i]], toremove)
     }
   }
-  if (length(CompleteOutlierDist$outgn)>0) {
-    genes2keep<-setdiff(gene.names, CompleteOutlierDist$outgn)
+  if (length(gn2rm)>0) {
+    genes2keep<-setdiff(gene.names, gn2rm)
     trees2<-list()
     j<-0
     for (i in 1:length(trees)) {
@@ -135,8 +144,10 @@ rm.gene.and.species.Distatis<-function(trees, sp2rm, gn2rm) {
         trees2[[j]]<-trees[[i]]
       }
     }
+    ##On conserve l'identifiant de chaque gène dans l'arbre de sortie.
    names(trees2)<-genes2keep
   }
+  ##Si gn2rm=0, on peut quand même enlever les espèces complete outlier
   else{
     trees2 = trees
   }
