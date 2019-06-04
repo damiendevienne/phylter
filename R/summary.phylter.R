c71a3f426247d25e257092425eff55c2b95f5e0c
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2019-06-04 16:40:27 +0200

    new summary and print method. Still missing doc

--- R/summary.phylter.R
+++ R/summary.phylter.R
@@ -1,4 +1,5 @@
 summary.phylter<-function(X) {
+	res<-NULL
 	percent.score.increase<-round((rev(X$Final$AllOptiScores)[1]-X$Final$AllOptiScores[1])*100, 2)
 	nb.outlier.cells<-nrow(X$Final$Outliers)
 	initial.nb.sp.per.mat<-unlist(lapply(X$Initial$mat.data, nrow))
@@ -6,12 +7,27 @@ summary.phylter<-function(X) {
 	nb.sp.removed.per.gene<-table(X$Final$Outliers[,1])[names(X$Initial$mat.data)]
 	nb.sp.removed.per.gene[is.na(nb.sp.removed.per.gene)]<-0
 	names(nb.sp.removed.per.gene)<-names(X$Initial$mat.data)
-	print(paste("Gain (concordance between matrices): ",percent.score.increase,"%", sep=""))
-	print(paste("Loss (data filtering): ",percent.data.filtered,"%", sep=""))
 	#complete outliers: 
 	ComplOutSP<-names(which(table(X$Final$Outliers[,2])==length(X$Final$weights)))
 	ComplOutGN<-names(which(table(X$Final$Outliers[,1])==length(X$Final$species.order)))
+	#result
+	res$nb.outlier.cells<-nb.outlier.cells
+	res$percent.score.increase<-percent.score.increase
+	res$percent.data.filtered<-percent.data.filtered
+	res$initial.nb.sp.per.mat<-initial.nb.sp.per.mat
+	res$nb.sp.removed.per.gene<-nb.sp.removed.per.gene
+	res$ComplOutGN<-ComplOutGN
+	res$ComplOutSP<-ComplOutSP
+	class(res)<-"summary.phylter"
+	res
+}
 
+print.summary.phylter<-function(x) {
+	if (!inherits(x, "summary.phylter"))
+		stop("'x' must inherit from class summar.phylter")
+	cat(paste("Number of outlier cells detected: ",x$nb.outlier.cells,"\n", sep=""))
+	cat(paste("Gain (concordance between matrices): ",x$percent.score.increase,"% \n", sep=""))
+	cat(paste("Loss (data filtering): ",x$percent.data.filtered,"% \n", sep=""))
 }
 
 print.phylter<-function(X) {
