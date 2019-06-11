c1babe397453b1de2f53cbebb807846d231368a3
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2019-06-11 16:02:55 +0200

    added description for dataset fungi

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' 246 genes trees for 21 fungal species
+#'
+#' A dataset containing all 246 gene trees explored in Aguileta et al. 2008, 
+#' for 21 fungal species covering the diversity of fungi. 
+#'
+#' @format A list of trees of class multiPhylo
+#' @source \url{https://academic.oup.com/sysbio/article/57/4/613/1632046}
+"fungi"
