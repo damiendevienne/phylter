4906efd1d4e6b4deabcda5f529a1573a01011134
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2022-03-14 16:09:24 +0100

    replaced dataset fungi for dataset carnivora

--- R/data.R
+++ R/data.R
@@ -1,8 +1,10 @@
-#' 246 genes trees for 21 fungal species
+#' 255 genes trees for 53 carnivora species
 #'
-#' A dataset containing all 246 gene trees explored in Aguileta et al. 2008, 
-#' for 21 fungal species covering the diversity of fungi. 
+#' This dataset contains 255 carnivora gene trees for 53 carnivora species. 
+#' It is a subset of the dataset used in Allio et al. (2021), obtained 
+#' by only keeping genes for which all 53 species were present. 
 #'
-#' @format A list of trees of class multiPhylo
-#' @source \url{https://academic.oup.com/sysbio/article/57/4/613/1632046}
-"fungi"
+#' @references Allio, R., Tilak, M. K., Scornavacca, C., Avenant, N. L., Kitchener, A. C., Corre, E., ... & Delsuc, F. (2021). High-quality carnivoran genomes from roadkill samples enable comparative species delineation in aardwolf and bat-eared fox. Elife, 10, e63167.
+#' @format A list of named trees of class multiPhylo
+#' @source \url{https://doi.org/10.7554/eLife.63167}
+"carnivora"
