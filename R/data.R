29a9aced5c69378d4eca3160f5f063ddde09a0bd
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2022-03-17 09:00:45 +0100

    Update an 'url' tag in a 'doi' one

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #'
 #' @references Allio, R., Tilak, M. K., Scornavacca, C., Avenant, N. L., Kitchener, A. C., Corre, E., ... & Delsuc, F. (2021). High-quality carnivoran genomes from roadkill samples enable comparative species delineation in aardwolf and bat-eared fox. Elife, 10, e63167.
 #' @format A list of named trees of class multiPhylo
-#' @source \url{https://doi.org/10.7554/eLife.63167}
+#' @source \doi{https://doi.org/10.7554/eLife.63167}
 #' @examples
 #' data(carnivora)
 #' class(carnivora)
