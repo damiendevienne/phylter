5d2967db144bb076bb51f1f1536a60c0025ba5b1
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2022-10-27 15:16:33 +0200

    new type of cell outliers addded: long branch outliers

--- R/simtrees.R
+++ R/simtrees.R
@@ -12,12 +12,17 @@
 #' @param Nsp Number of species (tips) per tree.
 #' @param Nsp.out Number of outlier species (also called rogue taxa). 0 = none.
 #' @param Ngn.out Number of outlier genes. 0 = none.
-#' @param Nb.cell.outlier Number of times one species in one tree is misplaced. 0 = none.
+#' @param Nb.cell.outlier Number of times one species in one tree is an outlier. 0 = none. The type of outlier is set by \code{out.type=}.
 #' @param brlen.sd Heterogeneity of branch lengths in trees. A value with mean 0 and standard
-#' deviation equal to brlen.sd is added to each branch length.
+#' deviation equal to brlen.sd is added to each branch length. If the resulting branch length has a negative value, its absolute value is taken.
+#' @param out.type The type of cell outlier. Can be "topology" (the default), where outlier species 
+#' are simulated by branching them elsewhere (randomly) in the tree, "brlength", where terminal branches of outlier species are 
+#' multiplied by \code{bl.mult} (see after) or "both" where half of the outliers are "topology" and the other half are "brlength".  
+#' In the latter case, if the number of outliers (\code{Nb.cell.outlier}) id odd, there is one more topology outlier than brlength outlier simulated.
+#' @param bl.mult Multiplier of terminal branches of outlier species when \code{out.type="topology"} or \code{out.type="both"}. Ignored otherwise.
 #' @return A list of trees in \code{multiPhylo} format.
 #' @examples  
-#' # Very basic simulator, for debuggin purpose mainly.
+#' # Very basic simulator, for debugging purpose mainly.
 #' # examples: 30 genes, 120 species, 2 outlier species, 3 outlier genes
 #' # 4 gene/species outliers, branch length variance = 0.6 
 #' trees<-simtrees(30,120,2,3,4,0.6)
@@ -27,7 +32,7 @@
 #' @importFrom ape rtree drop.tip Ntip bind.tree multi2di
 #' @importFrom stats rnorm
 #' @export
-simtrees<-function(Ngn, Nsp, Nsp.out=0,Ngn.out=0,Nb.cell.outlier=0, brlen.sd=0) {
+simtrees<-function(Ngn, Nsp, Nsp.out=0,Ngn.out=0,Nb.cell.outlier=0, brlen.sd=0, out.type="topology", bl.mult=2) {
   gen.trees<-function(Ntrees, Ntiptotal, Nspmove, NbweirdGenes, brlen.sd) {
     res<-list()
     selec<-NULL
@@ -69,7 +74,8 @@ simtrees<-function(Ngn, Nsp, Nsp.out=0,Ngn.out=0,Nb.cell.outlier=0, brlen.sd=0) 
     return(RES)
   }
 
-  add.outliers<-function(trees, nbrep) {
+
+  add.outliers<-function(trees, nbrep, type, mult) {
     move.tip<-function(tree, tip) {
       treesmall<-drop.tip(tree, tip)
       temptree<-rtree(2)
@@ -81,16 +87,51 @@ simtrees<-function(Ngn, Nsp, Nsp.out=0,Ngn.out=0,Nb.cell.outlier=0, brlen.sd=0) 
       treenew<-multi2di(treenew)
       return(treenew)
     }
+    expand.branch<-function(tree,tip, m) {
+      treenew<-tree
+      wheretip<-which(tree$tip.label==tip)
+      whereedge<-which(tree$edge[,2]==wheretip)
+      treenew$edge.length[whereedge]<-tree$edge.length[whereedge]*m
+      return(treenew)
+    }
     Ntiptotal<-Ntip(trees[[1]])
     Ntrees<-length(trees)
-    rrr<-c(NA,NA)
-    names(rrr)<-c("Species","Genes")
-    for (w in 1:nbrep) {
-      which.gn<-sample(1:Ntrees, 1)
-      which.sp<-paste("t",sample(1:Ntiptotal, 1),sep="")
-      rrr<-rbind(rrr,c(which.sp,which.gn))
-      trees[[which.gn]]<-move.tip(trees[[which.gn]], which.sp)
+    rrr<-c(NA,NA,NA)
+    names(rrr)<-c("Species","Genes", "type")
+
+    if (type=="topology") {
+      for (w in 1:nbrep) {
+        which.gn<-sample(1:Ntrees, 1)
+        which.sp<-paste("t",sample(1:Ntiptotal, 1),sep="")
+        rrr<-rbind(rrr,c(which.sp,which.gn, type))
+        trees[[which.gn]]<-move.tip(trees[[which.gn]], which.sp)
+      }
     }
+    if (type=="brlength") {
+      for (w in 1:nbrep) {
+        which.gn<-sample(1:Ntrees, 1)
+        which.sp<-paste("t",sample(1:Ntiptotal, 1),sep="")
+        rrr<-rbind(rrr,c(which.sp,which.gn, type))
+        trees[[which.gn]]<-expand.branch(trees[[which.gn]], which.sp, mult)
+      }
+    }
+    if (type=="both") {
+      nbrep1<-ceiling(nbrep/2)
+      nbrep2<-nbrep-nbrep1
+      for (w in 1:nbrep1) {
+        which.gn<-sample(1:Ntrees, 1)
+        which.sp<-paste("t",sample(1:Ntiptotal, 1),sep="")
+        rrr<-rbind(rrr,c(which.sp,which.gn, "topology"))
+        trees[[which.gn]]<-move.tip(trees[[which.gn]], which.sp)
+      }
+      for (w in 1:nbrep2) {
+        which.gn<-sample(1:Ntrees, 1)
+        which.sp<-paste("t",sample(1:Ntiptotal, 1),sep="")
+        rrr<-rbind(rrr,c(which.sp,which.gn, "brlength"))
+        trees[[which.gn]]<-expand.branch(trees[[which.gn]], which.sp, mult)
+      }
+    }
+
     RES<-NULL
     RES$trees<-trees
     RES$outl<-rrr[2:nrow(rrr),]  
@@ -101,7 +142,7 @@ simtrees<-function(Ngn, Nsp, Nsp.out=0,Ngn.out=0,Nb.cell.outlier=0, brlen.sd=0) 
     tr
   }
   tr<-gen.trees(Ngn,Nsp,Nsp.out,Ngn.out, brlen.sd)
-  if (Nb.cell.outlier>0) trees<-add.outliers(tr$trees, Nb.cell.outlier)$trees
+  if (Nb.cell.outlier>0) trees<-add.outliers(tr$trees, Nb.cell.outlier, out.type, bl.mult)$trees
   else trees<-tr$trees
   return(trees)
 }
